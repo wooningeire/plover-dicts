9088f30353c694e1bbd8bbe70577bd0b75ff7839
wooningeire <22846982+wooningeire@users.noreply.github.com>
2024-05-30 23:23:51 -0400

    feat: change hex entry to be char by char

--- plover-unicodepoint-dict/unicodepoint.py
+++ plover-unicodepoint-dict/unicodepoint.py
@@ -1,41 +1,70 @@
 from plover.steno import Stroke
 
-LONGEST_KEY = 2
+LONGEST_KEY = 8
 
-_ENTRY_STENO = "TKWHR"
+_ENTRY_STROKE = Stroke.from_steno("TKWHR")
+_EXIT_CHORD = Stroke.from_steno("R")
+_LEFT_SPACE_CHORD = Stroke.from_steno("A")
+_RIGHT_SPACE_CHORD = Stroke.from_steno("O")
 
 _key_values = {
-    key: 1 << i
-    for i, key in enumerate(reversed((
-        "-E", "-U",
-        "#", "S-", "T-", "K-",
-        "P-", "W-", "H-", "R-",
-        "-F", "-R", "-P", "-B",
-        "-L", "-G", "-T", "-S",
-    )))
+    "-R": 1 << 3,
+    "-B": 1 << 2,
+    "-G": 1 << 1,
+    "-S": 1 << 0,
+    "-U": 0,
 }
 
-def lookup(key):
-    # assert len(key) <= LONGEST_KEY, '%d/%d' % (len(key), LONGEST_KEY)
-    if _ENTRY_STENO != key[0]:
+def lookup(strokes_steno: tuple[str]):
+    entry_stroke = Stroke.from_steno(strokes_steno[0])
+    if entry_stroke != _ENTRY_STROKE:
         raise KeyError
-    if len(key) == 1:
-        return "u+"
+    
+
+    last_stroke = Stroke.from_steno(strokes_steno[-1])
     
     left_space = False
     right_space = False
+    has_exit_stroke = False
+    has_exit_chord_separated = False
+
+    if len(strokes_steno) >= 2:
+        if _LEFT_SPACE_CHORD in last_stroke:
+            last_stroke -= _LEFT_SPACE_CHORD
+            left_space = True
+        
+        if _RIGHT_SPACE_CHORD in last_stroke:
+            last_stroke -= _RIGHT_SPACE_CHORD
+            right_space = True
+
+        if _EXIT_CHORD in last_stroke:
+            if last_stroke == _EXIT_CHORD:
+                has_exit_chord_separated = True
+
+            last_stroke -= _EXIT_CHORD
+            has_exit_stroke = True
+    else:
+        return "u+"
+
 
     codepoint = 0
-    for key in Stroke.from_steno(key[1]).keys():
-        if key not in _key_values: 
-            if key == "A-":
-                left_space = True
-            if key == "O-":
-                right_space = True
+    for steno in strokes_steno[1:-1] if has_exit_chord_separated else strokes_steno[1:]:
+        codepoint <<= 4
 
-            continue
-        
-        codepoint += _key_values[key]
+        stroke = Stroke.from_steno(steno)
+        for key in stroke.keys():
+            if key not in _key_values:
+                raise KeyError
+            
+            codepoint += _key_values[key]    
+    valid_codepoint = codepoint < 0x110000
+
+
+    if not has_exit_stroke:
+        return f"u+{codepoint:x}{'' if valid_codepoint else ' [invalid]'}"
+    
+    if not valid_codepoint:
+        return f"[u+{codepoint:x}]"
 
     
     left_control = "{}" if left_space else "{^}"
